8b270661321c4de717f5c009604cce21a2cd59c0
GuessWhoLeagueSharp <lpaterso0479@gmail.com>
2016-12-07 23:12:06 -0800

    Add files via upload

---
+++ OKTWPredicitoner/Program.cs
@@ -0,0 +1,18 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace OKTWPredictioner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Game.Mode == GameMode.Running)
+            {
+                EventHandlers.Game_OnGameLoad(null);
+            }
+
+            CustomEvents.Game.OnGameLoad += EventHandlers.Game_OnGameLoad;
+        }
+    }
+}
