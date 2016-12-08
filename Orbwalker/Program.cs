4b681978fb628edb94b56366596e3b6112e213f4
GuessWhoLeagueSharp <lpaterso0479@gmail.com>
2016-12-07 23:10:21 -0800

    Add files via upload

---
+++ Orbwalker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LeagueSharp.Common;
+
+namespace StandaloneOrbwalker
+{
+    class Program
+    {
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        public static Menu Menu;
+
+        private static void Main()
+        {
+            CustomEvents.Game.OnGameLoad += OrbwalkerLoad;
+        }
+
+        private static void OrbwalkerLoad(EventArgs args)
+        {
+            Menu = new Menu("Standalone Orbwalker", "Standalone Orbwalker", true);
+            {
+                Menu orbwalkerMenu = Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+                Orbwalker = new Orbwalking.Orbwalker(orbwalkerMenu);
+                Menu.AddToMainMenu();
+            }
+        }
+    }
+}
